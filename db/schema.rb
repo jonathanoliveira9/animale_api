c184eec22fb35a91a57dccfa5ce52f97640b5458
jonathan <jonathanoliveirasilva9@gmail.com>
2021-11-23 20:35:01 -0200

    add model the communicator

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_215339) do
+ActiveRecord::Schema.define(version: 2021_11_23_223139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "animals", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "extra_information"
+    t.integer "status"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
