8298ebb5ee55700ab073e13c1ced051eee829619
jonathan <jonathanoliveirasilva9@gmail.com>
2021-11-23 19:08:51 -0200

    configure sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  Sidekiq::Web.use ActionDispatch::Cookies
+  Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: '_interslice_session'
+
+  mount Sidekiq::Web => '/sidekiq'
 end
