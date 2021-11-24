7bcdf902573e3db9252dd58851ad19823c507a61
jonathan <jonathanoliveirasilva9@gmail.com>
2021-11-24 00:55:47 -0200

    add routes animals api

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,10 @@ Rails.application.routes.draw do
   Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: '_interslice_session'
 
   mount Sidekiq::Web => '/sidekiq'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :animals, only: %w[index]
+    end
+  end
 end
