ab55e94791a2b81318a3c27923946b21c7ae6f1b
jonathan <jonathanoliveirasilva9@gmail.com>
2021-11-23 22:52:21 -0200

    add test to fields validate presence

--- spec/models/communicator_spec.rb
+++ spec/models/communicator_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Communicator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:communicator) }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without status' do
+    subject.phone = nil
+    expect(subject).to_not be_valid
+  end
 end
