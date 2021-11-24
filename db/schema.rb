9e719bb8ccc278a0f246a11572a8fb2fb8a7be9d
jonathan <jonathanoliveirasilva9@gmail.com>
2021-11-23 22:29:13 -0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_223139) do
+ActiveRecord::Schema.define(version: 2021_11_23_223405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2021_11_23_223139) do
     t.string "extra_information"
     t.integer "status"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "communicators", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.integer "animal_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
