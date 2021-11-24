d7416d2e1a049a1d2de39504ba2d4be3683d96dc
jonathan <jonathanoliveirasilva9@gmail.com>
2021-11-24 20:45:21 -0200

    test action create communicator

--- app/controllers/api/v1/communicators_controller.rb
+++ app/controllers/api/v1/communicators_controller.rb
@@ -2,5 +2,17 @@ class Api::V1::CommunicatorsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    animal = Communicator.new(communicator_params)
+    if animal.save
+      render json: animal, status: :created
+    else
+      render json: { errors: animal.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def communicator_params
+    params.require(:communicator).permit(:name, :phone, :animal_id)
   end
 end
