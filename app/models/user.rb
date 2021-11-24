0a94c65213688b0e333f0aa114cb70ff04b950c8
jonathan <jonathanoliveirasilva9@gmail.com>
2021-11-23 23:49:31 -0200

    add method devise jwt to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :jwt_authenticatable
+         :recoverable, :rememberable, :validatable, :jwt_authenticatable,
+         jwt_revocation_strategy: JwtDenylist
   has_many :animals
 
   validates :name, :email, :phone, :city, :state, presence: true
