1fe03787b033b6ee4a235fe086090a19d27e2a6f
jonathan <jonathanoliveirasilva9@gmail.com>
2021-11-23 23:49:42 -0200

    build routes devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   Sidekiq::Web.use ActionDispatch::Cookies
   Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: '_interslice_session'
