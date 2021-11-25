337a0d714cae800d72e494a6461f5b48c847a022
jonathan <jonathanoliveirasilva9@gmail.com>
2021-11-24 22:25:27 -0200

    Add Mailer

---
+++ app/mailers/animal_mailer.rb
@@ -0,0 +1,11 @@
+class AnimalMailer < ApplicationMailer
+  default from: 'animale@test.com'
+
+  def notify_changes(animal_id)
+    @animal = Animal.where(id: animal_id).last
+    @user = @animal.user
+
+    mail(to: @user.email,
+         subject: 'Novas atualizações de seu animal')
+  end
+end
