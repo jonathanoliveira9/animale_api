1dfadc85f3bd16eef25f3ea85ad5dcc39dfc8e0c
jonathan <jonathanoliveirasilva9@gmail.com>
2021-11-25 12:35:22 -0200

    configure access sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
   Sidekiq::Web.use ActionDispatch::Cookies
   Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: '_interslice_session'
 
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end if Rails.env.production?
+
   mount Sidekiq::Web => '/sidekiq'
 
   namespace :api, defaults: { format: :json } do
